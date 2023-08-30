1c17a4bea1f9b35eb22bd7e7823d4a4ca3b8e048
khushpreetsingh5911 <142646057+khushpreetsingh5911@users.noreply.github.com>
2023-08-30 16:36:30 +0000

    datatype

---
+++ 00_basics/5-datatype-summary.js
@@ -0,0 +1,17 @@
+//primitive datatype 
+// 7 types: string,number, boolean, null,undefined,symbol,bigInt
+const score= 299
+    const scorevalue= 322
+        const inLoggedin= false
+        const outsideTemp=null
+         let userEmai;
+             
+         const heroes=["shktiman","singh","mahakal"];
+         let myObj={
+            name:"hitesh",age:33,class:"4th"
+         }
+
+console.log(typeof myObj);
+
+//reference type(none primitive)
+//array,Objects,functions 
