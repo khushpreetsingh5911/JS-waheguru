63ff715cb2fac44b7b07e3121231543e593804b0
khushpreetsingh5911 <142646057+khushpreetsingh5911@users.noreply.github.com>
2023-08-30 05:00:25 +0000

    commit

---
+++ 00_basics/1_variables.js
@@ -0,0 +1,11 @@
+const accountId =14435
+let accountEmail ="khush@gmail.com"
+var accountPassword ="123456"
+accountCity = "muktsar"
+
+accountEmail= "khushbrar@gmail.com"
+accountPassword="2134234"
+
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity])
---
+++ 00_basics/2-datatypes.js
@@ -0,0 +1,13 @@
+// "use strict";
+ 
+// alert(waheguru) this will run in browser
+
+let age=4
+let Name="khush"
+let isLoggedin= true
+let state=null
+console.log(34+3);
+
+console.log(age, Name, state)
+
+console.log(typeof age)
