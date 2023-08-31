8415911f348dcd4107cd6818eca05f711b2351e8
khushpreetsingh5911 <142646057+khushpreetsingh5911@users.noreply.github.com>
2023-08-31 15:54:30 +0000

    array

--- 01-intermidate.js/1-array.js
+++ 01-intermidate.js/1-array.js
@@ -2,4 +2,22 @@ const myArry =[1,3,4,5,6,7 ,true, 'hitesh'];
 
 const newArry=['hitesh',"khush"];
 console.log(newArry)
+console.log(typeof(newArry))
 
+myArry.push(10)
+console.log(myArry)
+
+newArry.push(10)
+console.log(newArry)
+
+
+console.log("a", myArry)
+
+const mynewArry= myArry.slice(1,4)
+console.log(mynewArry)
+
+console.log("b", myArry)
+
+const narray=myArry.splice(1,4)
+console.log("c" , myArry)
+console.log(narray)
