03e1ffc86c5f39376ee5f44008b2ac3eae61e100
khushpreetsingh5911 <142646057+khushpreetsingh5911@users.noreply.github.com>
2023-08-31 08:28:23 +0000

    new

---
+++ 00_basics/8-dateandtime.js
@@ -0,0 +1,8 @@
+//dates
+
+let myDate=new Date()
+console.log(myDate.toISOString());
+let newdate= new Date(2023,8,32)
+console.log(newdate.toDateString())
+console.log(Date.now()/1000)
+console.log(Math.floor(Date.now()/1000))
