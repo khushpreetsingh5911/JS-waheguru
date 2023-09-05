de382fefb112ba7b719cacba362aa937500a49c3
khushpreetsingh5911 <142646057+khushpreetsingh5911@users.noreply.github.com>
2023-09-05 03:28:23 +0000

    while

---
+++ 05-interations/two.js
@@ -0,0 +1,26 @@
+
+let index=0
+while (index <= 11) {
+    console.log(`value of index is ${index}`)
+    index= index + 3
+}
+
+let myArray= ['flash','vatnna','spuderaq'
+
+]
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`)
+    arr++
+    
+}
+
+
+let score = 1;
+
+
+
+do {
+    console.log(`score is ${score}`);score++
+} while (score <= 10);
+
